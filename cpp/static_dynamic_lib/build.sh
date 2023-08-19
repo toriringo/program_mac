c02019bb8a324e452fb71a5f9035d355fbb06141
TORIUMI Mitsugu <toriringo@gmail.com>
2023-08-19 17:04:31 +0900

    modify

--- cpp/static_dynamic_lib/build.sh
+++ cpp/static_dynamic_lib/build.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-cd ./lib
-
 #静的ライブラリの作成
-g++ -c cat.cpp
-ar r ./static/libcat.a cat.o
+g++ -c ./lib/cat.cpp
+ar r ./lib/static/libcat.a ./lib/cat.o
 
 #動的ライブラリの作成
-g++ -shared cat.o -o ./dynamic/libcat.so
+g++ -shared ./lib/cat.o -o ./lib/dynamic/libcat.so
 #動的ライブラリのパスを通す
 export DYLD_LIBRARY_PATH="/Users/mittim/mittim/program/program/cpp/static_dynamic_lib/lib/dynamic"
-
-cd ..
 
 #静的ライブラリのリンク
 g++ main.cpp -L lib/static/ -l cat -o main_static
